706af74901f39ed52e4370e2bbcb73702b2dbada
HabchiSidAli <sidalihabchi96@gmail.com>
2023-08-11 18:07:03 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(arr) {
+    const result = arr.find(element =>{return element.result === "W"})
+    if (result === undefined) {
+        return result;
+    } else {
+        return result.year;
+    }
+}
